ded747924d53989c3f2c6daa95c9010a4e2cfb93
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-18 22:18:47 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+    if block_given?
+    result_array = []
+    counter = 0 
+    while counter >= collection.length 
+      for num in collection 
+      result_array << num if yield == true
+      counter += 1 
+    end
+    return result_array
+end
+end 
 end
